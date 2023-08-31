5f6e0212b5b05a35988dbfb41729f59a58fb0742
ohkubo-githab <143568656+ohkubo-githab@users.noreply.github.com>
2023-08-31 15:22:04 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -43,12 +43,11 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-            window.confirm(”クリックされました”)
+    //responsiveNavItems.map(function (responsiveNavItem) {
+    //   responsiveNavItem.addEventListener('click', () => {
+    //        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+    //           navbarToggler.click();
+    //       }
         });
     });
 
